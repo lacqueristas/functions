f734f5146e0a11a0e2971a3ada6a7fb1af4c3749
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-03-28 00:28:21 -0700

    Attempting to see if image magick will give us the mimetype

--- source/correctContentTypeForImages/index.js
+++ source/correctContentTypeForImages/index.js
@@ -1,19 +1,39 @@
 import googleStorage from "@google-cloud/storage"
+import magick from "gm"
+import {propEq} from "ramda"
 
 const googleStorageClient = googleStorage()
+const imageMagick = magick.subClass({imageMagick: true})
+
+const isDelete = propEq("resourceState", "not_exists")
+const isOctetStream = propEq("contentType", "application/octet-stream")
 
 export default function correctContentTypeForImages (event: Object, callback: Function): any {
   const {data} = event
-  const {name} = event
-  const {ContentType} = event
-  const {resourceState} = data
-  const isDelete = resourceState === "not_exists"
+  const {name} = data
+  const {bucket} = data
 
-  if (isDelete) {
+  if (isDelete(data)) {
     return callback()
   }
 
-  console.log({event, data})
+  if (isOctetStream(data)) {
+    const from = googleStorageClient
+      .bucket(bucket)
+      .file(name)
+      .createReadStream()
+
+    return imageMagick(from)
+      .identify({bufferStream: true}, function format (error: any, information: Object): any {
+        if (error) {
+          return callback()
+        }
+
+        console.log({information})
+
+        return callback()
+      })
+  }
 
   return callback()
 }
--- transpiled/correctContentTypeForImages/index.js
+++ transpiled/correctContentTypeForImages/index.js
@@ -9,23 +9,43 @@ var _storage = require("@google-cloud/storage");
 
 var _storage2 = _interopRequireDefault(_storage);
 
+var _gm = require("gm");
+
+var _gm2 = _interopRequireDefault(_gm);
+
+var _ramda = require("ramda");
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var googleStorageClient = (0, _storage2.default)();
+var imageMagick = _gm2.default.subClass({ imageMagick: true });
+
+var isDelete = (0, _ramda.propEq)("resourceState", "not_exists");
+var isOctetStream = (0, _ramda.propEq)("contentType", "application/octet-stream");
 
 function correctContentTypeForImages(event, callback) {
   var data = event.data;
-  var name = event.name;
-  var ContentType = event.ContentType;
-  var resourceState = data.resourceState;
+  var name = data.name;
+  var bucket = data.bucket;
 
-  var isDelete = resourceState === "not_exists";
 
-  if (isDelete) {
+  if (isDelete(data)) {
     return callback();
   }
 
-  console.log({ event: event, data: data });
+  if (isOctetStream(data)) {
+    var from = googleStorageClient.bucket(bucket).file(name).createReadStream();
+
+    return imageMagick(from).identify({ bufferStream: true }, function format(error, information) {
+      if (error) {
+        return callback();
+      }
+
+      console.log({ information: information });
+
+      return callback();
+    });
+  }
 
   return callback();
 }
