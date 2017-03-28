200acb6a6a42a844967f8bbcaf945a5f9150fab1
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-03-27 21:45:28 -0700

    Working towards eating from google storage

--- source/correctContentTypeForImages/index.js
+++ source/correctContentTypeForImages/index.js
@@ -1,3 +1,17 @@
-export default function correctContentTypeForImages (): any {
-  return console.log("Hello, world")
+import googleStorage from "@google-cloud/storage"
+
+const googleStorageClient = googleStorage()
+
+export default function correctContentTypeForImages (event: Object, callback: Function): any {
+  const {data} = event
+  const {resourceState} = data
+  const isDelete = resourceState === "not_exists"
+
+  if (isDelete) {
+    return callback()
+  }
+
+  console.log(data)
+
+  return callback()
 }
--- transpiled/correctContentTypeForImages/index.js
+++ transpiled/correctContentTypeForImages/index.js
@@ -4,6 +4,26 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = correctContentTypeForImages;
-function correctContentTypeForImages() {
-  return console.log("Hello, world");
+
+var _storage = require("@google-cloud/storage");
+
+var _storage2 = _interopRequireDefault(_storage);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var googleStorageClient = (0, _storage2.default)();
+
+function correctContentTypeForImages(event, callback) {
+  var data = event.data;
+  var resourceState = data.resourceState;
+
+  var isDelete = resourceState === "not_exists";
+
+  if (isDelete) {
+    return callback();
+  }
+
+  console.log(data);
+
+  return callback();
 }
